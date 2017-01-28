d658499665ec6b12b4725dc149abb0d894ca0efc
Sebastian Hoß <mail@shoss.de>
2017-01-29 00:30:17 +0100

    prepare for other databases
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- yosql-example/src/main/java/com/example/app/ExampleApp.java
+++ yosql-example/src/main/java/com/example/app/ExampleApp.java
@@ -1,5 +1,6 @@
 package com.example.app;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -10,27 +11,31 @@ import com.example.persistence.CompanyRepository;
 public class ExampleApp {
 
     public static final void main(final String[] arguments) {
-        final JdbcDataSource ds = new JdbcDataSource();
-        ds.setURL("jdbc:h2:mem:db1;DB_CLOSE_DELAY=-1");
+        if (Arrays.stream(arguments).anyMatch("h2"::equals)) {
+            final JdbcDataSource ds = new JdbcDataSource();
+            ds.setURL("jdbc:h2:mem:db1;DB_CLOSE_DELAY=-1");
 
-        final CompanyRepository companyRepository = new CompanyRepository(ds);
-        companyRepository.createCompaniesTable();
-        companyRepository.insertCompany(123, "test");
-        companyRepository.queryAllCompanies().forEach(System.out::println);
-        System.out.println("----------------------");
-        companyRepository.batchInsertCompany(new int[] { 456, 789 }, new String[] { "two", "three" });
-        companyRepository.streamEagerQueryAllCompanies().forEach(System.out::println);
-        System.out.println("----------------------");
-        companyRepository.batchInsertCompany(new int[] { 91, 32 }, new String[] { "more", "other" });
-        try (Stream<Map<String, Object>> companies = companyRepository.streamLazyQueryAllCompanies()) {
-            companies.forEach(System.out::println);
+            final CompanyRepository companyRepository = new CompanyRepository(ds);
+            companyRepository.createCompaniesTable();
+            companyRepository.insertCompany(123, "test");
+            companyRepository.queryAllCompanies().forEach(System.out::println);
+            System.out.println("----------------------");
+            companyRepository.batchInsertCompany(new int[] { 456, 789 }, new String[] { "two", "three" });
+            companyRepository.streamEagerQueryAllCompanies().forEach(System.out::println);
+            System.out.println("----------------------");
+            companyRepository.batchInsertCompany(new int[] { 91, 32 }, new String[] { "more", "other" });
+            try (Stream<Map<String, Object>> companies = companyRepository.streamLazyQueryAllCompanies()) {
+                companies.forEach(System.out::println);
+            }
+            System.out.println("----------------------");
+            companyRepository.insertCompany(47, "test");
+            companyRepository.findCompanyByName("test").forEach(System.out::println);
+            System.out.println("----------------------");
+            companyRepository.batchInsertCompany(new int[] { 56, 78 }, new String[] { "abc", "def" });
+            companyRepository.findCompaniesWithinRange(30, 95).forEach(System.out::println);
+        } else if (Arrays.stream(arguments).anyMatch("psql"::equals)) {
+            // start psql first
         }
-        System.out.println("----------------------");
-        companyRepository.insertCompany(47, "test");
-        companyRepository.findCompanyByName("test").forEach(System.out::println);
-        System.out.println("----------------------");
-        companyRepository.batchInsertCompany(new int[] { 56, 78 }, new String[] { "abc", "def" });
-        companyRepository.findCompaniesWithinRange(30, 95).forEach(System.out::println);
     }
 
 }
