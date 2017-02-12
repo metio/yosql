009fdd37bb92054ea79843a9282fdf6c9ba6c256
Sebastian Hoß <mail@shoss.de>
2017-02-12 18:32:13 +0100

    add log4j impl
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- yosql-maven-plugin/src/main/java/com/github/sebhoss/yosql/generator/logging/Log4jLoggingGenerator.java
+++ yosql-maven-plugin/src/main/java/com/github/sebhoss/yosql/generator/logging/Log4jLoggingGenerator.java
@@ -1,50 +1,87 @@
 package com.github.sebhoss.yosql.generator.logging;
 
-import com.github.sebhoss.yosql.generator.LoggingGenerator;
-import com.squareup.javapoet.CodeBlock;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+
+import com.github.sebhoss.yosql.generator.LoggingGenerator;
+import com.github.sebhoss.yosql.generator.helpers.TypicalFields;
+import com.github.sebhoss.yosql.generator.helpers.TypicalNames;
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.TypeName;
+
+@Named
+@Singleton
 public class Log4jLoggingGenerator implements LoggingGenerator {
+
+    private final TypicalFields fields;
+
+    @Inject
+    public Log4jLoggingGenerator(final TypicalFields fields) {
+        this.fields = fields;
+    }
+
+    public FieldSpec logger(final TypeName repoClass) {
+        return fields.prepareConstant(getClass(), Logger.class, TypicalNames.LOGGER)
+                .initializer("$T.getLogger($T.class.getName())", Logger.class, repoClass)
+                .build();
+    }
 
     @Override
     public CodeBlock queryPicked(final String fieldName) {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.finer(() -> String.format($S, $S))", TypicalNames.LOGGER, "Picked query [%s]",
+                        fieldName)
+                .build();
     }
 
     @Override
     public CodeBlock indexPicked(final String fieldName) {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.finer(() -> String.format($S, $S))", TypicalNames.LOGGER,
+                        "Picked index [%s]", fieldName)
+                .build();
     }
 
     @Override
     public CodeBlock vendorQueryPicked(final String fieldName) {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.finer(() -> String.format($S, $S))", TypicalNames.LOGGER,
+                        "Picked query [%s]", fieldName)
+                .build();
     }
 
     @Override
     public CodeBlock vendorIndexPicked(final String fieldName) {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.finer(() -> String.format($S, $S))", TypicalNames.LOGGER,
+                        "Picked index [%s]", fieldName)
+                .build();
     }
 
     @Override
     public CodeBlock vendorDetected() {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.fine(() -> $T.format($S, $N))", TypicalNames.LOGGER, String.class,
+                        "Detected database vendor [%s]", TypicalNames.DATABASE_PRODUCT_NAME)
+                .build();
     }
 
     @Override
     public CodeBlock executingQuery() {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder()
+                .addStatement("$N.fine(() -> $T.format($S, $N))", TypicalNames.LOGGER, String.class,
+                        "Executing query [%s]", TypicalNames.EXECUTED_QUERY)
+                .build();
     }
 
     @Override
     public CodeBlock shouldLogLow() {
-        // TODO Auto-generated method stub
-        return null;
+        return CodeBlock.builder().add("$N.isLoggable($T.FINE)", TypicalNames.LOGGER, Level.class).build();
     }
 
 }
