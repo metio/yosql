8214c60777c4bdac86cb43beeeb083fe8a30068c
Sebastian Hoß <mail@shoss.de>
2017-02-21 16:37:23 +0100

    remove unused class
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- yosql-maven-plugin/src/main/java/com/github/sebhoss/yosql/error/ErrorReporter.java
+++
@@ -1,7 +0,0 @@
-package com.github.sebhoss.yosql.error;
-
-/**
- * Error reporting should be in its own package because users can redirect log messages from that package into its own log file.
- */
-public class ErrorReporter {
-}
