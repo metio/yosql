47b05eb4345f455520383eed4bc269d75bc36a4b
Sebastian Hoß <mail@shoss.de>
2017-04-01 15:38:33 +0200

    add more config options for cli

--- yosql-cli/java/de/xn__ho_hia/yosql/YoSqlCLI.java
+++ yosql-cli/java/de/xn__ho_hia/yosql/YoSqlCLI.java
@@ -7,6 +7,8 @@
 package de.xn__ho_hia.yosql;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import de.xn__ho_hia.yosql.cli.PathValueConverter;
 import de.xn__ho_hia.yosql.generator.AnnotationGenerator;
@@ -30,6 +32,7 @@ import de.xn__ho_hia.yosql.generator.utils.ResultStateGenerator;
 import de.xn__ho_hia.yosql.generator.utils.ToResultRowConverterGenerator;
 import de.xn__ho_hia.yosql.model.ExecutionConfiguration;
 import de.xn__ho_hia.yosql.model.ExecutionErrors;
+import de.xn__ho_hia.yosql.model.LoggingAPI;
 import de.xn__ho_hia.yosql.parser.PathBasedSqlFileResolver;
 import de.xn__ho_hia.yosql.parser.SqlConfigurationFactory;
 import de.xn__ho_hia.yosql.parser.SqlFileParser;
@@ -60,17 +63,16 @@ public class YoSqlCLI {
     private static ExecutionConfiguration createConfiguration(final String[] args) {
         final ValueConverter<Path> pathConverter = new PathValueConverter();
         final OptionParser parser = new OptionParser();
+        final Path currentDirectory = Paths.get(".");
         final OptionSpec<Path> inputBaseDirectory = parser.accepts("inputBaseDirectory")
                 .withRequiredArg()
-                .ofType(String.class)
-                .defaultsTo(".")
                 .withValuesConvertedBy(pathConverter)
+                .defaultsTo(currentDirectory)
                 .describedAs("The input directory for .sql files");
         final OptionSpec<Path> outputBaseDirectory = parser.accepts("outputBaseDirectory")
                 .withRequiredArg()
-                .ofType(String.class)
-                .defaultsTo(".")
                 .withValuesConvertedBy(pathConverter)
+                .defaultsTo(currentDirectory)
                 .describedAs("The output directory for all generated files");
         final OptionSpec<String> basePackageName = parser.accepts("basePackageName")
                 .withRequiredArg()
@@ -100,16 +102,69 @@ public class YoSqlCLI {
         final OptionSpec<String> sqlFilesCharset = parser.accepts("sqlFilesCharset")
                 .withRequiredArg()
                 .ofType(String.class)
+                .defaultsTo("UTF-8")
                 .describedAs("The character set to use while reading .sql files.");
         final OptionSpec<String> sqlStatementSeparator = parser.accepts("sqlStatementSeparator")
                 .withRequiredArg()
                 .ofType(String.class)
+                .defaultsTo(";")
                 .describedAs(
                         "The SQL statement separator to use to split multiple SQL statement inside a single .sql file.");
-        parser.accepts("defaultRowConverter");
+        final OptionSpec<String> defaultRowConverter = parser.accepts("defaultRowConverter")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo(ToResultRowConverterGenerator.RESULT_ROW_CONVERTER_ALIAS)
+                .describedAs(
+                        "The default row converter to use for each statement that returns a value and does not specify a converter itself. Can either be the alias of a converter or the fully qualified name of a converter.");
+        final OptionSpec<String> methodBatchPrefix = parser.accepts("methodBatchPrefix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("")
+                .describedAs("The method name prefix to apply to all batch methods.");
+        final OptionSpec<String> methodBatchSuffix = parser.accepts("methodBatchSuffix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("Batch")
+                .describedAs("The method name suffix to apply to all batch methods.");
+        final OptionSpec<String> methodStreamPrefix = parser.accepts("methodStreamPrefix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("")
+                .describedAs("The method name prefix to apply to all stream methods.");
+        final OptionSpec<String> methodStreamSuffix = parser.accepts("methodStreamSuffix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("Stream")
+                .describedAs("The method name suffix to apply to all stream methods.");
+        final OptionSpec<String> methodRxJavaPrefix = parser.accepts("methodRxJavaPrefix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("")
+                .describedAs("The method name prefix to apply to all stream methods.");
+        final OptionSpec<String> methodRxJavaSuffix = parser.accepts("methodRxJavaSuffix")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("Flow")
+                .describedAs("The method name suffix to apply to all stream methods.");
+        final OptionSpec<String> methodEagerName = parser.accepts("methodEagerName")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("Eager")
+                .describedAs("The method name suffix to apply to all eager stream methods.");
+        final OptionSpec<String> methodLazyName = parser.accepts("methodLazyName")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("Lazy")
+                .describedAs("The method name suffix to apply to all lazy stream methods.");
+        final OptionSpec<Boolean> generateStandardApi = parser.accepts("generateStandardApi")
+                .withRequiredArg()
+                .ofType(Boolean.class)
+                .defaultsTo(Boolean.TRUE)
+                .describedAs(
+                        "Controls whether the generated repositories should contain standard methods that. Standard methods execute depending on the type of the query and could either be a single 'executeQuery' on a PreparedStatement in case of SQL SELECT statements or a single call to 'executeUpdate' for SQL UPDATE statements.");
 
         final OptionSet options = parser.parse(args);
-        final int javaVersion = Integer.parseInt(options.valueOf(java));
+        final int javaVersion = parseJavaVersion(options.valueOf(java));
 
         return YoSql.prepareDefaultConfiguration()
                 .setInputBaseDirectory(options.valueOf(inputBaseDirectory))
@@ -118,11 +173,48 @@ public class YoSqlCLI {
                 .setUtilityPackageName(options.valueOf(utilityPackageName))
                 .setConverterPackageName(options.valueOf(converterPackageName))
                 .setRepositoryNameSuffix(options.valueOf(repositoryNameSuffix))
+                .setMethodBatchPrefix(options.valueOf(methodBatchPrefix))
+                .setMethodBatchSuffix(options.valueOf(methodBatchSuffix))
+                .setMethodStreamPrefix(options.valueOf(methodStreamPrefix))
+                .setMethodStreamSuffix(options.valueOf(methodStreamSuffix))
+                .setMethodRxJavaPrefix(options.valueOf(methodRxJavaPrefix))
+                .setMethodRxJavaSuffix(options.valueOf(methodRxJavaSuffix))
+                .setMethodEagerName(options.valueOf(methodEagerName))
+                .setMethodLazyName(options.valueOf(methodLazyName))
                 .setSqlFilesCharset(options.valueOf(sqlFilesCharset))
                 .setSqlStatementSeparator(options.valueOf(sqlStatementSeparator))
+                .setGenerateStandardApi(options.valueOf(generateStandardApi).booleanValue())
+                .setGenerateBatchApi(true)
                 .setGenerateStreamEagerApi(javaVersion >= 8)
                 .setGenerateStreamLazyApi(javaVersion >= 8)
+                .setGenerateRxJavaApi(true)
+                .setAllowedCallPrefixes(Arrays.asList("call", "execute"))
+                .setAllowedReadPrefixes(Arrays.asList("select", "read", "query", "find"))
+                .setAllowedWritePrefixes(Arrays.asList("update", "insert", "delete", "create", "write", "add", "remove",
+                        "merge", "drop"))
+                .setValidateMethodNamePrefixes(true)
+                .setMethodCatchAndRethrow(true)
+                .setClassGeneratedAnnotation(true)
+                .setFieldGeneratedAnnotation(true)
+                .setMethodGeneratedAnnotation(true)
+                .setRepositoryGenerateInterface(true)
+                .setGeneratedAnnotationComment("DO NOT EDIT")
+                .setLoggingApi(LoggingAPI.JDK)
+                .setDefaulFlowStateClassName(FlowStateGenerator.FLOW_STATE_CLASS_NAME)
+                .setDefaultResultStateClassName(ResultStateGenerator.RESULT_STATE_CLASS_NAME)
+                .setDefaultResultRowClassName(ResultRowGenerator.RESULT_ROW_CLASS_NAME)
+                .setDefaultRowConverter(options.valueOf(defaultRowConverter))
                 .build();
+    }
+
+    private static int parseJavaVersion(final String givenJavaVersion) {
+        final String[] versionParts = givenJavaVersion.split("\\."); //$NON-NLS-1$
+        String versionToParse = versionParts[0];
+        if (versionParts.length > 1) {
+            versionToParse = versionParts[1];
+        }
+        final int javaVersion = Integer.parseInt(versionToParse);
+        return javaVersion;
     }
 
     private static YoSql createYoSql(final ExecutionConfiguration configuration) {
