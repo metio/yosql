24e79b5b6e6adb2e6468c88bc8065ebf2c94d2ba
Sebastian Hoß <mail@shoss.de>
2017-05-12 22:12:04 +0200

    add more validation tests
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- yosql-core/src/test/java/de/xn__ho_hia/yosql/generator/helpers/TypicalCodeBlocksTest.java
+++ yosql-core/src/test/java/de/xn__ho_hia/yosql/generator/helpers/TypicalCodeBlocksTest.java
@@ -1,11 +1,15 @@
 package de.xn__ho_hia.yosql.generator.helpers;
 
+import java.util.Arrays;
+
 import com.squareup.javapoet.CodeBlock;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import de.xn__ho_hia.yosql.model.SqlConfiguration;
+import de.xn__ho_hia.yosql.model.SqlParameter;
 import de.xn__ho_hia.yosql.testutils.ValidationFile;
 import de.xn__ho_hia.yosql.testutils.ValidationFileParameterResolver;
 
@@ -43,6 +47,66 @@ class TypicalCodeBlocksTest {
         // given
         // when
         final CodeBlock codeBlock = TypicalCodeBlocks.getColumnCount();
+
+        // then
+        Assertions.assertEquals(validationFile.read(), codeBlock.toString());
+    }
+
+    @Test
+    public void shouldExecuteQuery(final ValidationFile validationFile)
+            throws Exception {
+        // given
+        // when
+        final CodeBlock codeBlock = TypicalCodeBlocks.executeQuery();
+
+        // then
+        Assertions.assertEquals(validationFile.read(), codeBlock.toString());
+    }
+
+    @Test
+    public void shouldExecuteUpdate(final ValidationFile validationFile)
+            throws Exception {
+        // given
+        // when
+        final CodeBlock codeBlock = TypicalCodeBlocks.executeUpdate();
+
+        // then
+        Assertions.assertEquals(validationFile.read(), codeBlock.toString());
+    }
+
+    @Test
+    public void shouldPrepareBatch(final ValidationFile validationFile)
+            throws Exception {
+        // given
+        final SqlConfiguration config = new SqlConfiguration();
+        final SqlParameter parameter = new SqlParameter();
+        parameter.setName("test");
+        config.setParameters(Arrays.asList(parameter));
+
+        // when
+        final CodeBlock codeBlock = TypicalCodeBlocks.prepareBatch(config);
+
+        // then
+        Assertions.assertEquals(validationFile.read(), codeBlock.toString());
+    }
+
+    @Test
+    public void shouldExecuteBatch(final ValidationFile validationFile)
+            throws Exception {
+        // given
+        // when
+        final CodeBlock codeBlock = TypicalCodeBlocks.executeBatch();
+
+        // then
+        Assertions.assertEquals(validationFile.read(), codeBlock.toString());
+    }
+
+    @Test
+    public void shouldAddBatch(final ValidationFile validationFile)
+            throws Exception {
+        // given
+        // when
+        final CodeBlock codeBlock = TypicalCodeBlocks.addBatch();
 
         // then
         Assertions.assertEquals(validationFile.read(), codeBlock.toString());
