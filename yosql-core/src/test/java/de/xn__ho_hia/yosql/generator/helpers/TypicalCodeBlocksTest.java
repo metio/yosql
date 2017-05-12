452b8b8de06a043fceaac5fc202cb543f2efe539
Sebastian Hoß <mail@shoss.de>
2017-05-12 23:30:13 +0200

    use static imports
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- yosql-core/src/test/java/de/xn__ho_hia/yosql/generator/helpers/TypicalCodeBlocksTest.java
+++ yosql-core/src/test/java/de/xn__ho_hia/yosql/generator/helpers/TypicalCodeBlocksTest.java
@@ -1,5 +1,7 @@
 package de.xn__ho_hia.yosql.generator.helpers;
 
+import static de.xn__ho_hia.yosql.generator.helpers.TypicalCodeBlocks.*;
+
 import org.junit.jupiter.api.Test;
 
 import de.xn__ho_hia.yosql.testutils.TestSqlConfigurations;
@@ -11,42 +13,42 @@ class TypicalCodeBlocksTest extends ValidationFileTest {
 
     @Test
     public void shouldAssignFieldToValueWithSameName(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.setFieldToSelf("test"), validationFile);
+        validate(setFieldToSelf("test"), validationFile);
     }
 
     @Test
     public void shouldGetMetadataFromResultSet(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.getMetaData(), validationFile);
+        validate(getMetaData(), validationFile);
     }
 
     @Test
     public void shouldGetColumnCountFromMetadata(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.getColumnCount(), validationFile);
+        validate(getColumnCount(), validationFile);
     }
 
     @Test
     public void shouldExecuteQuery(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.executeQuery(), validationFile);
+        validate(executeQuery(), validationFile);
     }
 
     @Test
     public void shouldExecuteUpdate(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.executeUpdate(), validationFile);
+        validate(executeUpdate(), validationFile);
     }
 
     @Test
     public void shouldPrepareBatch(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.prepareBatch(TestSqlConfigurations.createConfiguration()), validationFile);
+        validate(prepareBatch(TestSqlConfigurations.createConfiguration()), validationFile);
     }
 
     @Test
     public void shouldExecuteBatch(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.executeBatch(), validationFile);
+        validate(executeBatch(), validationFile);
     }
 
     @Test
     public void shouldAddBatch(final ValidationFile validationFile) {
-        validate(TypicalCodeBlocks.addBatch(), validationFile);
+        validate(addBatch(), validationFile);
     }
 
 }
