836b843a460e9ee36f6d0ab78b0795e7f13a2af6
Sebastian Hoß <sebhoss@pm.me>
2021-03-20 18:59:26 +0100

    add ConstructorTCK

---
+++ yosql-codegen/yosql-codegen-tck/src/main/java/wtf/metio/yosql/codegen/tck/ConstructorTCK.java
@@ -0,0 +1,29 @@
+/*
+ * This file is part of yosql. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of yosql,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+package wtf.metio.yosql.codegen.tck;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import wtf.metio.yosql.codegen.api.ConstructorGenerator;
+import wtf.metio.yosql.testing.configs.Sql;
+
+public interface ConstructorTCK {
+
+    ConstructorGenerator generator();
+
+    String forRepositoryExpectation();
+
+    @Test
+    default void forRepository() {
+        Assertions.assertEquals(
+                forRepositoryExpectation(),
+                generator().forRepository(Sql.sqlStatements()).toString(),
+                "The generated constructor did not match expectation");
+    }
+
+}
