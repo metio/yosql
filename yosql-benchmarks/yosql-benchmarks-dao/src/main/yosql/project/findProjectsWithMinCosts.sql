39b2589181322c0a8886dca7a1e9d4371f77e36c
Sebastian Hoß <seb@ho.de>
2022-05-22 18:20:29 +0200

    add missing .sql files

---
+++ yosql-benchmarks/yosql-benchmarks-dao/src/main/yosql/project/findProjectsWithMinCosts.sql
@@ -0,0 +1,21 @@
+/*
+ * This file is part of yosql. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at https://creativecommons.org/publicdomain/zero/1.0/. No part of yosql,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+-- parameters:
+--   - name: cost
+--     type: long
+SELECT  p.*
+FROM    projects p
+    INNER JOIN (
+        SELECT  pe.project_pid, sum(e.salary) as cost
+        FROM    projectEmployees pe
+            INNER JOIN employees e
+               ON pe.employee_pid = e.pid
+        GROUP BY pe.project_pid
+    ) data ON p.pid = data.project_pid
+WHERE   data.cost >= :cost
+;
---
+++ yosql-benchmarks/yosql-benchmarks-dao/src/main/yosql/projectEmployee/insertProjectEmployee.sql
@@ -0,0 +1,15 @@
+/*
+ * This file is part of yosql. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at https://creativecommons.org/publicdomain/zero/1.0/. No part of yosql,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+-- parameters:
+--   - name: project
+--     type: long
+--   - name: employee
+--     type: long
+INSERT INTO projectEmployees (project_pid, employee_pid)
+VALUES (:project, :employee)
+;
